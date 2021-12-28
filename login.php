19d2d1676d4f01e59a4e6cc51788e14e7ab59d90
DanielHartzema <91454288+DanielHartzema@users.noreply.github.com>
2021-12-28 23:56:17 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" type="image/x-icon" href="img/Logo ICON White.png">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://use.fontawesome.com/1a4d35d4d9.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Sorting Hat</title>
+</head>
+<body>
+  <?php
+    include 'assets/includes/nav.php';
+  ?>
+  <a id="button"></a>
+
+  <div class="parallaxKlein">
+      <div class="TekstMiddenKlein">
+          <h1 class="groot">Log-In</h1>
+      </div>
+  </div>
+
+  <div class="tekst" id="contact">
+    <section id="contact">
+      <h1 class="section-header">Log In</h1>
+      
+    </section>  
+  </div>
+  
+  <script src="assets/js/script.js"></script>
+
+  <?php
+    include 'assets/includes/footer.php';
+  ?>
+
+</body>
+</html> 
