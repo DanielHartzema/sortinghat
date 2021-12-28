e017082a3a179d2fb5aa3d41f163a9c9d2d4cc01
DanielHartzema <91454288+DanielHartzema@users.noreply.github.com>
2021-12-28 23:56:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/Logo ICON White.png">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://use.fontawesome.com/1a4d35d4d9.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Sorting Hat</title>
+    <?php include 'assets/includes/connect.php'; ?>
+</head>
+<body>
+  <?php
+    include 'assets/includes/nav.php';
+  ?>
+
+  <a id="button"></a>
+
+  <div class="parallax">
+      <div class="TekstMidden">
+          <h1 class="groot">Welcome</h1>
+          <hr>
+          <h4>Log-in or create an account to vote</h5>
+      </div>
+  </div>
+
+  <script src="Assets/js/script.js"></script>
+
+<?php
+  include 'assets/includes/footer.php';
+?>
+
+</body>
+</html> 
